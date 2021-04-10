583be5c4fc0eec647e679c00f46cd03f914ca1e0
Douglas Miller <durgadas311@gmail.com>
2021-04-10 17:40:47 -0500

    Update SC203 wiznet boot to use NVRAM

--- rom/wizmod.asm
+++ rom/wizmod.asm
@@ -3,6 +3,14 @@
 	extrn	last
 	extrn	NTWKIN, NTWKST, CNFTBL, SNDMSG, RCVMSG, NTWKER, NTWKBT, NTWKDN
 
+	public	nvbuf	; for libwznet
+	extrn	wizcfg	; from libwznet
+
+conout	equ	0040h
+
+CR	equ	13
+LF	equ	10
+
 	cseg
 first:
 	db	'n','W'
@@ -10,7 +18,7 @@ first:
 	dw	descr-first
 
 ;	Jump vector for SNIOS entry points
-	jmp	NTWKIN	; network initialization
+	jmp	wizinit	; network initialization
 	jmp	NTWKST	; network status
 	jmp	CNFTBL	; return config table addr
 	jmp	SNDMSG 	; send message on network
@@ -21,7 +29,23 @@ first:
 	; nothing else in cseg here...
 	; SNIOS config table must be here
 
+wizinit:
+	; initialize W5500 from NVRAM, if present
+	call	wizcfg
+	lxi	h,wizerr
+	cc	msgout
+	jmp	NTWKIN
+
+msgout:	mov	a,m
+	ora	a
+	rz
+	mov	c,a
+	call	conout
+	jmp	msgout
+
 	dseg
 descr:	db	'MT011 WizNet',0
+wizerr:	db	'No NVRAM confiuration',CR,LF,0
+nvbuf:	ds	512
 
 	end
